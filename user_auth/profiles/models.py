7d3630bb7169d15e3baf9de6dee94ee98d9080cc
Onikah <mona.kwamboka@gmail.com>
2016-04-10 23:17:07 +0300

    allauth added

--- user_auth/profiles/models.py
+++ user_auth/profiles/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+class profile(models.Model):
+	name = models.CharField(max_length = 1200)
+	description = models.TextField(default = 'description default')
+	
+	def __unicode__(self):
+		return self.name
--- user_auth/user_auth/settings.py
+++ user_auth/user_auth/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -63,10 +67,18 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request',
             ],
         },
     },
 ]
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 WSGI_APPLICATION = 'user_auth.wsgi.application'
 
@@ -119,3 +131,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ID = 1
