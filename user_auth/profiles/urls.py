40167bad645235710f3b4e52ad6e02d1f2acb0e1
skafis <franciskipchumba5@gmail.com>
2016-04-18 21:12:16 +0300

    map urls to render view profile template

--- user_auth/profiles/urls.py
+++ user_auth/profiles/urls.py
@@ -4,19 +4,19 @@ from profiles import views
 
 admin.autodiscover()
 
-
-
 urlpatterns = [
-        url(r'^$', views.index, name='index'),
-        url(r'^usertype/$', views.usertype, name="usertype"),
-        url(r'^helper/$', views.helper, name="helper"),
-        url(r'^seeker/$', views.seeker, name="seeker"),
-        url(r'^location/$', views.location, name='location'),
-        url(r'^skills/$', views.skills, name='skills'),
-        url(r'^days/$', views.days, name='days'),
-        url(r'^create/$', views.create_opportunity_form, name = "create"),
-        url(r'^browse/$', views.browse, name = "browse"),
-        url(r'current/$', views.current_opportunities, name="current_opportunities"),
-        url(r'browse/opportunity/(?P<id>[0-9]+)/$', views.helper_request, name="helper_request"),
-        url(r'opportunity/(?P<id>[0-9]+)/$', views.single_request, name="single_request"),
-        ]
+    url(r'^$', views.index, name='index'),
+    url(r'^usertype/$', views.usertype, name="usertype"),
+    url(r'^helper/$', views.helper, name="helper"),
+    url(r'^seeker/$', views.seeker, name="seeker"),
+    url(r'^location/$', views.location, name='location'),
+    url(r'^skills/$', views.skills, name='skills'),
+    url(r'^days/$', views.days, name='days'),
+    url(r'^create/$', views.create_opportunity_form, name="create"),
+    url(r'^browse/$', views.browse, name="browse"),
+    url(r'current/$', views.current_opportunities, name="current_opportunities"),
+    url(r'browse/opportunity/(?P<id>[0-9]+)/$', views.helper_request, name="helper_request"),
+    url(r'opportunity/(?P<id>[0-9]+)/$', views.single_request, name="single_request"),
+    url(r'profile/$', views.view_profile, name="profile"),
+
+]
